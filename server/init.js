063826a5490e3d87d4dd85c996b611478e3f3510
AndrMinakoV <borishasanov591@gmail.com>
2023-02-12 19:23:22 +0200

    upd

--- server/init.js
+++ server/init.js
@@ -4,6 +4,6 @@ console.log(process.env.MONGODB_URL)
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGODB_URL);
 
-// bot.launch(
-//   console.log('\x1b[36m%s\x1b[0m', `Клиент запущен. Подгруженные команды:`)
-// );
+ bot.launch(
+   console.log('\x1b[36m%s\x1b[0m', `Клиент запущен. Подгруженные команды:`)
+ );
